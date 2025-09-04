7185233b15bfb452c6fcddd5f6e40d04197e0fd3
Michał Puchała <michalpuchala86@gmail.com>
2025-09-04 18:06:57 +0200

    fix problem

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import { strContains } from '../utils/strContains';
 import listsReducer from './listsRedux';
 import columnsReducer from './columnsRedux';
 import cardsReducer from './cardsRedux';
 import searchStringReducer from './searchStringRedux';
-
-export const getFilteredCards = ({ cards, searchString }, columnId) => 
-  cards.filter(card => card.columnId === columnId && strContains(card.title, searchString)
-);
-
-export const getAllColumns = state => state.columns;
-
-export const addColumn = (title, icon, listId) => ({ type: 'ADD_COLUMN', payload: { title, icon, listId} });
-
-export const addCard = (title, columnId) => ({ type: 'ADD_CARD', payload: { title, columnId }});
-
-export const searchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
-
-export const getListById = ({lists}, listId) => lists.find(list => listId === list.id);
-
-export const getColumnsByList = ({columns} ,listId) => columns.filter(column => column.listId === listId);
-
-export const getAllLists = state => state.lists;
-
-export const addFavoriteStyle = payload => ({ type: 'ADD_FAVORITE_STYLE', payload});
-
-export const favoriteCard = state => state.cards.filter(card => !card.isFavorite === true);
-
-export const addList = (title, description) => ({ type: 'ADD_LIST', payload: { title, description}});
- 
 
 /*const reducer = (state, action) => {
   const newState = {
